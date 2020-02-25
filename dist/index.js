1d9906bb858c8de3f7422659d7bffe33def5863f
Ian Marsh <i-marsh@github.com>
2020-02-25 18:46:25 -0500

    update distribution - confirms executed version - must build dist

--- dist/index.js
+++ dist/index.js
@@ -196,6 +196,7 @@ try {
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log('***********');
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
